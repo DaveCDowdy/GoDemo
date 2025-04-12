c6b381551cf52658de7da060a12b4f1187aaecc3
DaveCDowdy <thedaviddowdy@gmail.com>
2025-04-12 00:58:15 -0500

    aggregate types

--- main.go
+++ main.go
@@ -6,9 +6,22 @@ import (
 )
 
 func main() {
-	name, score := "Dent, Arthur", 87
+
+	type score struct {
+		name string
+		score int
+	}
+
+	scores := []score{
+		{name: "Dent, Arthur", score: 87},
+		{name: "McMillan, Tricia", score: 96},
+		{name: "Prefect, Ford", score: 64},
+	}
+	
 
 	fmt.Println("Student scores")
 	fmt.Println(strings.Repeat("-", 14))
-	fmt.Println(name, score)
+	fmt.Println(scores[0].name, scores[0].score)
+	fmt.Println(scores[1].name, scores[1].score)
+	fmt.Println(scores[2].name, scores[2].score)
 }
